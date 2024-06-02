156f367be897cdf9fc789978a2ce18a9330e6215
Leah Nguyen <nduongthucanh@gmail.com>
2024-06-02 14:45:50 +1000

    debug control flows

--- assets/script.js
+++ assets/script.js
@@ -1,19 +1,27 @@
 const topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
+const randomTopic = topics[Math.floor(Math.random() * topics.length)];
 
-for (let x = 0; x < topics.length; x++) {
-    console.log(topics[x]);
+function listTopics() {
+    for (let x = 0; x < topics.length; x++) {
+        console.log(topics[x]);
+    }
 }
 
-/*
-if (topic === 'HTML') {
-    console.log("Let's study HTML!");
-} else if (topic === 'CSS') {
-    console.log("Let's study CSS!");
-} else if (topic === 'Git') {
-    console.log("Let's study Git!");
-} else if (topic === 'JavaScript') {
-    console.log("Let's study JavaScript!");
-} else {
-    console.log('Please try again!');
+function selectTopic() {
+    if (topic === 'HTML') {
+        console.log("Let's study HTML!");
+    } else if (topic === 'CSS') {
+        console.log("Let's study CSS!");
+    } else if (topic === 'Git') {
+        console.log("Let's study Git!");
+    } else if (topic === 'JavaScript') {
+        console.log("Let's study JavaScript!");
+    } else {
+        console.log('Please try again!');
+    }
 }
-*/
+
+console.log('Here are the topics we learned through Prework:');
+listTopics();
+console.log('Which topic should we study first?');
+selectTopic();
